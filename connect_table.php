b6c0d9e130fdd583e4dbd5101adb7b81d33960d1
Mad-Jelly <apivat_c@hotmail.com>
2021-10-14 11:58:18 +0700

    เก็บข้อมูลเชื่อมต่อเว็ปไว้ ไม่ให้ขึ้น git

--- connect_table.php
+++ connect_table.php
@@ -9,12 +9,3 @@ mysqli_select_db($con,$dbname);
 ?>
 
 
-<?php
-/*$hostname="163.44.198.60";
-$username="cp706962_mid";
-$password="kmx-2510";
-$dbname="cp706962_mid_log";
-$con=mysqli_connect($hostname,$username,$password);
-mysqli_query($con,"SET NAMES UTF8");
-mysqli_select_db($con,$dbname);*/
-?>
